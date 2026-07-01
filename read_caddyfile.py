39b0e17f05d29505a9790d6deff705a70c032d3d
Konstantin <crahercenter@gmail.com>
2026-07-02 04:03:28 +0600

    chore: remove temporary diagnostic scripts

--- read_caddyfile.py
+++
@@ -1,9 +0,0 @@
-import paramiko
-client = paramiko.SSHClient()
-client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-client.connect('158.255.4.142', username='root', password='Kostaden2312-')
-sftp = client.open_sftp()
-sftp.get('/opt/safescan/Caddyfile', 'Caddyfile.remote.current')
-sftp.close()
-client.close()
-print(open('Caddyfile.remote.current').read())
